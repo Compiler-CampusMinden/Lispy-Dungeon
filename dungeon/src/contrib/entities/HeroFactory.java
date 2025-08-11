e007958007c6b5f675d353f8b033e5f41258df6e
Flamtky <niklas.schumann+github@proton.me>
2025-08-11 16:31:22 +0200

    allow only unique players | fix player fireball for now | refactor

--- dungeon/src/contrib/entities/HeroFactory.java
+++ dungeon/src/contrib/entities/HeroFactory.java
@@ -15,6 +15,7 @@ import contrib.utils.components.skill.SkillTools;
 import core.Entity;
 import core.Game;
 import core.components.*;
+import core.game.PreRunConfiguration;
 import core.level.Tile;
 import core.network.messages.c2s.InputMessage;
 import core.network.messages.c2s.InputMessage.Action;
@@ -116,7 +117,7 @@ public final class HeroFactory {
    * @throws IOException if the animation could not been loaded.
    */
   public static Entity newHero() throws IOException {
-    return newHero(HERO_DEATH, true, "Player");
+    return newHero(HERO_DEATH, true, PreRunConfiguration.username());
   }
 
   public static Entity newHero(boolean isLocal, String playerName) throws IOException {
@@ -140,8 +141,9 @@ public final class HeroFactory {
    * @return A new Entity.
    * @throws IOException if the animation could not been loaded.
    */
-  public static Entity newHero(Consumer<Entity> deathCallback, boolean isLocal, String playerName) throws IOException {
-    Entity hero = new Entity("hero");
+  public static Entity newHero(Consumer<Entity> deathCallback, boolean isLocal, String playerName)
+      throws IOException {
+    Entity hero = new Entity("hero_" + playerName);
     hero.persistent(true);
     PlayerComponent pc = new PlayerComponent(isLocal, playerName);
     hero.add(pc);
